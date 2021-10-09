1076a1bb03d850fb30a1215b63c021e6bdb65dbc
hafizmfadli <lshafizmf1st@gmail.com>
2021-10-09 16:54:49 +0700

    Membuat tampilan home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <menu-option btnTitle='Buat Poll' imgName='logo.svg' />
+      </v-col>
+      <v-col
+        cols="12"
+        md="6"
+      >
+       <menu-option btnTitle='Ikuti Poll' imgName='logo.png' />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
+  import MenuOption from '../components/MenuOption.vue'
   export default {
     name: 'Home',
     components: {
-      HelloWorld,
+      MenuOption
     },
   }
 </script>
